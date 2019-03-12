52ee65f07de22028faaff722ac9a43ae252dab1e
karoldabek <email>
2019-03-12 13:33:10 +0100

    tlumaczenie2

--- python/tlumacz.py
+++ python/tlumacz.py
@@ -2,19 +2,21 @@
 # -*- coding: utf-8 -*-
 #
 #  tlumacz.py
-
+from random import randint
+import os
+import json
 
 def pokaz_menu():
     """Funkcja wyświetla działania dostępne dla użytkownia"""
     print("""
     Wybierz działanie:
-
     1. Lista słów
     2. Wprowadzanie / edycja słów
     3. Tłumaczenie
     4. Zmień język
     5. Koniec
 """)
+
 
 def listaSlow(dane):
     if not dane:
@@ -25,6 +27,7 @@ def listaSlow(dane):
     for slowo, znaczenia in dane.items():
         print('{}. {}: {}'.format(i, slowo, ','.join(dane[slowo])))
         i += 1
+
 
 def pobierzZnaczenia():
     znaczenia = input('Podaj znaczenie(a) oddzielone przecinkami:\n')
@@ -43,9 +46,60 @@ def pobierzDane(dane):
     else:
         dane[slowo] = pobierzZnaczenia()
 
+def tlumacz(dane):
+    if not dane:
+        print('Brak słów.')
+        return
+    slowa = lista(dane.keys())
+    op = 't'
+    while op == 't':
+        if len(slowa) > 1:
+            slowo = slowa[randint(0, len(slowa) - 1)]
+        else:
+            slowo = slowa[0]
+        print('Przetłumacz: ', slowo)
+        znaczenia = pobierzZnaczenia()
+        poprawne = [z for z in znaczenia if z in dane[slowo]]
+        if poprawne:
+            print('Poprawne:', ', '.join(poprawne))
+            slowa.remove(slowo)
+        else:
+            print('Brak poprawnych znaczeń')
+        if slowa:
+            op = input('Następne (t/n)? ').lower()
+            print()
+        else:
+            print('Przrtłumaczyłeś wszystko!')
+            return
+            
+
+def wczytaj_dane(plik, roz = '.dat'):
+    dane = {}
+    if os.path.isfile(plik + roz):
+        with open(plik + roz, "r") as f:
+            dane = json.load(f)
+    else:
+        print('Plik {} nie istnieje.'.format(plik + roz))
+    return dane
+
+def wybierzJezyk(konf_dane):
+    if konf_dane['jezyki']:
+        print('Wybierz język: ')
+        for i, j in enumerate(konf_dane['jezyk']):
+            print('{}. {}'.format(i + 1, j))
+        print('{}. nowy język'.format(i + 2))
+
 
 def main(args):
-    dane = {'go': ['iść', 'jeździć'], 'see': ['widzieć', 'oglądać']}
+    #dane ={'go': ['iść', 'jeździć'], 'see': ['widzieć', 'oglądać']}
+    
+    konf_plik = 'baza'
+    konf_dane = wczytaj_dane(konf_plik)
+    if 'jenzyki' not in konf_dane:
+        konf_dane['jezyki'] = []
+    jezyk = wybierzJezyk(konf_dane)
+    print(konf_dane)
+    return
     
     operacja = 0
     while operacja != 5:
@@ -53,12 +107,15 @@ def main(args):
         operacja = int(input(""))
         if operacja == 1:
             listaSlow(dane)
-        if operacja == 2:
-            listaSlow(dane)
+        elif operacja == 2:
+            pobierzDane(dane)
+        elif operacja == 3:
+            pobierzDane(dane)
         elif operacja == 5:
             print('\nDo zobaczenia!')
         else:
             print('Błędny wybór!')
+            
     return 0
 
 if __name__ == '__main__':
