a4fc16e06098194250557354327144aef3d6a91f
karoldabek <karolina.dabek@lo1.sandomierz.pl>
2019-09-19 11:40:49 +0200

    czw 11:40 19 września

---
+++ cpp/trojkat.cpp
@@ -0,0 +1,28 @@
+/*
+ * trojkat.cpp
+ * napisz program który pobiera od użytkownika trzy liczby, 
+ * długoiść boków i sprawdz czy da się z niech zrobić trójkąt
+ * Copyright 2019 kl2ag1 <kl2ag1@ubu10>
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a;
+        cout << "Podja liczbę a: ";
+    int b;
+        cout << "Podja liczbę b: ";
+    int c;
+        cout << "Podja liczbę c: ";
+    cin >> a >> b >> c;
+    if (a + b > c && a + c > b && b + c > a);{
+        cout << "trójkąt <3" << endl;}
+    else {cout << "nie trójkąt" ;}
+    
+	return 0;
+}
+
