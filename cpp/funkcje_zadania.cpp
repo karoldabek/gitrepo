dd130183baf522cb95026836ca9dbd942511bd8a
karoldabek <karolina.dabek@lo1.sandomierz.pl>
2019-10-07 10:35:30 +0200

    głodna jestem

---
+++ cpp/funkcje_zadania.cpp
@@ -0,0 +1,28 @@
+/*
+ * funkcje_zadania.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int drukuj(int a, int b, int c) {
+    cout << "Pracujesz? Powątpiewam" << a << "ile lat i ile zarabiasz" << b << endl;
+}
+
+int awans(staz, &zarobek){
+    staz += 1;
+    zarobek
+}
+
+int main(int argc, char **argv)
+{
+    int staz = 1;
+    int zarobek = 1000;
+    int lata = 0;
+    cout << "przewidywany okres pracy w latach?" << endl;
+    cin >> lata;
+	return 0;
+}
+
