e596e61a4e99883ddac9976eb83f7ce7e815a087
karoldabek <karolina.dabek@lo1.sandomierz.pl>
2019-10-11 09:40:13 +0200

    app

--- public_html/phpapp/indeks.php
+++ public_html/phpapp/indeks.php
@@ -1,5 +1,23 @@
-<?php
+<?php //w tym plitu nie będzie coś tam główny definujeny :P
 
-phpinfo();
+phpinfo('DINC', 'inc/'); //DING nazwa zmiennej, inc/ ścieżka względna
+phpinfo('DBASE', 'baza/');
+&dbfile = 'db.sqlite3';
+&db = null;
+require_once(DINC.'functions.php');
+require_once(DINC.'db.php');
+require_once(DINC.'users.php');
+
+if(isset(&_GET['id']))
+	&id = &_GET['id'];
+else
+	&id = 'Witam';
+
+includ_once(DINC.'template.php')
+
+
+
+//ścieżka bezwzględna pokazuje ścieszke gdzie znajduje się ten plik (od home)
+//ścieka względna od naszego pliku
 
 ?>
