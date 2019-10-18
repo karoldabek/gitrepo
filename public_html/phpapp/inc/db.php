7a0c937cabd2a3a5a0151fff44ced82044e67c72
karoldabek <karolina.dabek@lo1.sandomierz.pl>
2019-10-18 09:40:35 +0200

    app sqlite

--- public_html/phpapp/inc/db.php
+++ public_html/phpapp/inc/db.php
@@ -1,3 +1,11 @@
 <?php
+function init_baza(){
+	global $db, $dbfile;
+	if(!file_exists(DBASE.$dbfileb)) {
+		$kom[] = 'Brak pliku baza. Tworzę nowy.';
+		$db = new PDO("sqlite:DBASE.$dbfile");
+	}
+		$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
 ;
 ?>
--- public_html/phpapp/indeks.php
+++ public_html/phpapp/indeks.php
@@ -1,8 +1,14 @@
 <?php //w tym plitu nie będzie coś tam główny definujeny :P
+setlocale(LC_ALL, 'pl_PL.UTF-8');
+date_default_timezone_set('Europe/Warsaw');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', 'errorlog.txt');
 
 phpinfo('DINC', 'inc/'); //DING nazwa zmiennej, inc/ ścieżka względna
-phpinfo('DBASE', 'baza/');
-&dbfile = 'db.sqlite3';
+phpinfo('DBASE', 'db/');
+&dbfile = 'baza.db';
 &db = null;
 require_once(DINC.'functions.php');
 require_once(DINC.'db.php');
