116735ef7ed5097eb64f2af64a8c2c3111c4c73d
karoldabek <karolina.dabek@lo1.sandomierz.pl>
2019-10-24 11:40:32 +0200

    tablicka :P

---
+++ cpp/tablica2wmnożenie.cpp
@@ -0,0 +1,33 @@
+/*
+ * tablica2w.cpp
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#define N   10  //stałe ozaczamy dużymi literami np. N, M
+#define M   10
+
+int main(int argc, char **argv)
+{
+	int tab2w[N][M];
+    int i, j;
+    
+    //generator liczb psełko losowych (jest to pewin algorytm zakres od 0 do 1) :
+    srand(time(NULL));               // (time(NULL)) - początek epoki juniksa(jest przectawiona jaki 32-bitowa liczba sekund, która upłynęła od 1 stycznie 1970 roku)
+                                     // inicjacja generatora psełdo losowych
+    for (i=0; i<N; i++){
+        cout << "========= Tablica " << i << " =========" << endl;
+        for (j=0; j<M; j++){
+            cout << 1 << " " << j << endl;
+            tab2w[i][j] = rand() % 101;
+            cout << setw(4) << tab2w[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    
+	return 0;
+}
+
