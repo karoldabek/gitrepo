7ea688db01d2f28ff3de954e22c25190e4142de3
karoldabek <karolina.dabek@lo1.sandomierz.pl>
2019-11-28 11:40:43 +0100

    dokończyć pf

--- cpp/reszta.cpp
+++ cpp/reszta.cpp
@@ -15,9 +15,66 @@
 #include <iostream>
 using namespace std;
 
+int nominaly[100];
+int liczbaNominalow;
+
+void wczytajNominaly() {
+    cout << "Podaj liczbę nominałów: ";
+    cin >> liczbaNominalow;
+    
+    cout << "Podaj nominały w kolejności malejacje: " << endl;
+    int i=0;
+    int nominal = 0;
+    cin >> nominal;
+    niminaly[i] = niminal;
+    i++;
+    while (i < liczbaNominalow){
+        cin >> nominal;
+        if (niminal > nominaly[i-1]){
+            cout << "Nominał większy od poprzedniego!!" << endl;
+            continue;
+        }
+        nominaly[i] = niminal;
+        i++;
+    }
+    //~ for(int i = 0; i < liczbaNominalow; i++){
+        //~ cin >> nominaly[i];
+        //~ }
+    //~ for(int i = 0; i < liczbaNominalow; i++){
+        //~ cout << nominaly[i] << " ";
+        //~ }
+    //~ cout << endl;
+    }
+
+int znajdzReszte(int reszta){
+    cout << "Wplacone nominały: " << endl;
+    int aktualnyNominal = 0;
+    
+    while (warunek > 0 && aktualnyNominal < liczbaNominalow){
+        cout << "reszta = " << reszta << endl;
+        //znajdz największy dostepny nominal mniejszy od reszty;
+        cout << "aktualny nominał: " << nominaly[aktualnyNominal] << endl;
+        //oblicz wymaganych ilośc nominałów
+        //pomnijsz reszte
+        }
+    
+    }
+
 int main(int argc, char **argv)
 {
-	
+	wczytajNominaly();
+    int cena;
+    int wplaaca;
+    cout << "Podaj cenę: ";
+    cin >> cena;
+    
+    do { //pętla blokującja
+        cout << "Podaj wplatę: ";
+        cin >> wplata;
+    } while(wplata < cena);
+    
+    znajdzReste(wplata - cena);
+    
 	return 0;
 }
 
