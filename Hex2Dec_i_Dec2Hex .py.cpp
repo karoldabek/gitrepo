565327a52ca4725fb444083a935b682eaf30961a
karoldabek <karolina.dabek@lo1.sandomierz.pl>
2020-02-10 10:35:15 +0100

    bin i nie bin

---
+++ Hex2Dec_i_Dec2Hex .py.cpp
@@ -0,0 +1,21 @@
+/*
+ * Hex2Dec_i_Dec2Hex .py.cpp
+ * 
+ * pobierzasz zyskujesz
+ * program czyta 
+ * jeżeli sprawdzasz poszczegulen znaki
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+	int = tab[4];
+    cout << "Podaj znaczki (max 4)" << endl;
+    
+	return 0;
+}
+
